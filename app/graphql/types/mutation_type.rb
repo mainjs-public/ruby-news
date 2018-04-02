408c21f5d600ab62b0fabe67031b0354d8553634
Dang Tien Ngoc <dangtienngoc@hotmail.com>
2018-04-02 23:57:52 +0700

    mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,4 +8,22 @@ Types::MutationType = GraphQL::ObjectType.define do
       "Hello World!"
     }
   end
+
+  field :createPerson, Types::PersonType do
+  	description "Create a person"
+  	
+  	argument :first_name, !types.String
+  	argument :last_name, !types.String
+  	argument :email, !types.String
+  	argument :notes, !types.String
+
+  	resolve ->(obj, args, ctx) {
+        Person.create!(
+		   first_name: args[:first_name],
+		   last_name: args[:last_name],
+		   email: args[:email],
+		   notes: args[:notes],
+  		)
+    }
+  end
 end
