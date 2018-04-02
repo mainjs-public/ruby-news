1a6c38d0ff290b33720d464259d8ce464b3ac884
Dang Tien Ngoc <dangtienngoc@hotmail.com>
2018-04-02 20:12:33 +0700

    GraphQL query

---
+++ app/graphql/types/person_type.rb
@@ -0,0 +1,10 @@
+Types::PersonType = GraphQL::ObjectType.define do
+	# this type is named 'Link'
+	name 'Person'
+
+	field :id, !types.ID
+	field :first_name, !types.String
+	field :last_name, !types.String
+	field :email, !types.String
+	field :notes, !types.String
+end
