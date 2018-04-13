16dece4e6a597471e42f55d08602b2e0d1bbaa6d
Dang Tien Ngoc <dangtienngoc@hotmail.com>
2018-04-13 12:01:21 +0700

    Update category

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -4,10 +4,10 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-        # Query context goes here, for example:
-        session: session,
-        current_user: current_user,
-        #
+      # Query context goes here, for example:
+      session: session,
+      # current_user: current_user,
+      #
     }
     result = New24Schema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -16,17 +16,17 @@ class GraphqlController < ApplicationController
   private
 
   # gets current user from token stored in session
-  def current_user
-    # if we want to change the sign-in strategy, this is the place todo it
-    return unless session[:token]
-
-    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
-    token = crypt.decrypt_and_verify session[:token]
-    user_id = token.gsub('user-id:', '').to_i
-    User.find_by id: user_id
-  rescue ActiveSupport::MessageVerifier::InvalidSignature
-    nil
-  end
+  # def current_user
+  #   # if we want to change the sign-in strategy, this is the place todo it
+  #   return unless session[:token]
+  #
+  #   crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
+  #   token = crypt.decrypt_and_verify session[:token]
+  #   user_id = token.gsub('user-id:', '').to_i
+  #   User.find_by id: user_id
+  # rescue ActiveSupport::MessageVerifier::InvalidSignature
+  #   nil
+  # end
 
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
